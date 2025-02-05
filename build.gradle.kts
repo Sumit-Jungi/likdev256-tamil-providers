f4f3b50bdadeb6c3470056977a07ddddd51b6dce
Krunal Gajjar <kgajjar44@gmail.com>
2025-02-05 13:51:17 +0530

    Updated kotlin and gradle version and changed site url

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,10 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("com.android.tools.build:gradle:8.1.0")
         // Cloudstream gradle plugin which makes everything work and builds plugins
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
 
@@ -41,6 +41,7 @@ subprojects {
     }
 
     android {
+        namespace = "com.likdev256"
         defaultConfig {
             minSdk = 21
             compileSdkVersion(33)
